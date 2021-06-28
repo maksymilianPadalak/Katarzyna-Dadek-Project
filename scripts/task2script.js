7f465188394d9edd50180f7ef87c2db13c2048a7
Maksymilian Padalak <mpadalak@edu.cdv.pl>
2021-06-28 20:47:42 +0200

    Latest commit
    latest commit

--- scripts/task2script.js
+++ scripts/task2script.js
@@ -1,26 +1,41 @@
-const pierwszy = document.getElementById("pierwszy")
-const drugi = document.getElementById("drugi")
-const trzeci = document.getElementById("trzeci")
-const task2Text = document.getElementById("task2-text")
-const colors = ['pink', 'yellow', 'green', 'blue', 'red']
+const pierwszy = document.getElementById("pierwszy");
+const drugi = document.getElementById("drugi");
+const trzeci = document.getElementById("trzeci");
+const task2Text = document.getElementById("task2-text");
+const task2Letters = document.querySelector("#task2-text");
+const textInnerHtml = task2Letters.innerHTML;
+
+const colors = ["pink", "yellow", "green", "blue", "red"];
+
+let colorChangeInterval;
+
+const changeColor = () => {
+  task2Text.style.color = colors[getRandomInt(colors.length)];
+};
 
 getRandomInt = (max) => {
-    return Math.floor(Math.random() * max);
+  return Math.floor(Math.random() * max);
+};
+
+pierwszy.addEventListener("change", () => {
+  clearInterval(colorChangeInterval);
+  task2Letters.innerHTML = textInnerHtml;
+  task2Text.style.color = "black";
+});
+
+drugi.addEventListener("change", () => {
+  task2Letters.innerHTML = textInnerHtml;
+  changeColor();
+  globalThis.colorChangeInterval = setInterval(changeColor, 3000);
+});
+
+trzeci.addEventListener("change", () => {
+  clearInterval(colorChangeInterval);
+  let newElement = "";
+  for (let i = 0; i < task2Text.textContent.length; i++) {
+    newElement += `<span style="color: rgb(${getRandomInt(255)}, ${getRandomInt(
+      255
+    )}, ${getRandomInt(255)});">${task2Text.textContent[i]}</span>`;
   }
-  
-
-pierwszy.addEventListener('change', () =>{
-    task2Text.style.color = 'black'
-})
-
-drugi.addEventListener('change', () =>{
-    task2Text.style.color = colors[getRandomInt(colors.length)]
-    const colorText = setInterval(function(){
-        task2Text.style.color = colors[getRandomInt(colors.length)]
-    }, 3000)
-})
-
-// trzeci.addEventListener('change', () =>{
-//     task2Text.innerText[3].style.color = `rgb(${getRandomInt(255)}, ${getRandomInt(255)}, ${getRandomInt(255)})`
-// })
-
+  task2Letters.innerHTML = newElement;
+});
